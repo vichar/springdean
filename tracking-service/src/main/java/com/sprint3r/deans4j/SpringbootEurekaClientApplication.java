bf82250b09d10ffaf77421ea23acf4aeb9cbf9be
Nuttawut Singhabut <bootnuttawig@gmail.com>
2017-03-05 16:05:00 +0700

    Update progress

--- tracking-service/src/main/java/com/sprint3r/deans4j/SpringbootEurekaClientApplication.java
+++ tracking-service/src/main/java/com/sprint3r/deans4j/SpringbootEurekaClientApplication.java
@@ -1,6 +1,6 @@
 package com.sprint3r.deans4j;
 
-import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,27 +24,40 @@ public class SpringbootEurekaClientApplication {
 	@Autowired
     JdbcTemplate jdbcTemplate;
 	
+	@PostMapping("/updateTrackingNumber")
+	String updateTrackingNumber(Long orderId, String tackingNumber) {
+		
+		Optional<Tracking> t = repo.findByOrderId(orderId);
+		Tracking tracking = t.get();
+		tracking.setTackingNumber(tackingNumber);
+		repo.save(tracking);
+		return "success";
+	}
+	
+	@PostMapping("/updateTrackingStatus")
+	String updateTrackingStatus(Long orderId, String status) {
+		
+		Optional<Tracking> t = repo.findByOrderId(orderId);
+		Tracking tracking = t.get();
+		tracking.setStatus(status);
+		repo.save(tracking);
+		return "success";
+	}
+	
+	//UpdateTrackingNumber
+	
+	
 	@GetMapping("/getTrackingStatusByOrderId")
-	Iterable<Tracking> genTrackingNoByOrderId() {
-		Iterable<Tracking> list = jdbcTemplate.query("select * from Tracking t WHERE t.orderId = ?", new Object[]{1},
-    			
-    			(rs, rowNum) ->
-		        
-		    	new Tracking(
-		                rs.getLong("id"),
-		                rs.getString("tackingNumber"),
-		                rs.getDate("sentDate"),
-		                rs.getLong("orderId"))
-        ); 
-    	
-        return list;
+	Iterable<Tracking> genTrackingNoByOrderId(Long orderId) {
+		return repo.findAllOrderId(orderId);
 	}
 
 	@Bean
 	public InitializingBean seedDatabase(TrackingRepository repository) {
 		System.out.println("initial DB");
 		return () -> {
-			repository.save(new Tracking("EMS123456777", new Date(), 1L));
+			repository.save(new Tracking("EMS123456777", "Packing", 1L));
+			repository.save(new Tracking("KEERY9999", "Packing", 2L));
 		};
 	}
 
--- tracking-service/src/main/java/com/sprint3r/deans4j/TrackingRepository.java
+++ tracking-service/src/main/java/com/sprint3r/deans4j/TrackingRepository.java
@@ -1,7 +1,13 @@
 package com.sprint3r.deans4j;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TrackingRepository extends CrudRepository<Tracking, Long> {
 
+	Optional<Tracking> findByOrderId(Long orderId);
+	List<Tracking> findAllOrderId(Long orderId);
+	
 }
