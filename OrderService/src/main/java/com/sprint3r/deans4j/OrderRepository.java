09648a7319373d96b2559924242956d69ec93a75
poringbom <poringbom@PoringbomZA>
2017-03-05 16:25:04 +0700

    update

--- OrderService/src/main/java/com/sprint3r/deans4j/OrderRepository.java
+++ OrderService/src/main/java/com/sprint3r/deans4j/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.sprint3r.deans4j;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "order", path = "order")
 public interface  OrderRepository extends  CrudRepository<Order,Long>{
 
 }
