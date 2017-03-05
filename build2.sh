5fd0ee65a9562a499d29576acc5f8d34b89641ff
hatoriz <ninja.hatoriz@gmail.com>
2017-03-05 15:21:14 +0700

    [TRAVIS] - fix error

--- build2.sh
+++ build2.sh
@@ -1,10 +1,8 @@
 #bin/bash
 echo "springboot-data-jpa"
 ls -la
-cd ./springboot-data-jpa
-./gradlew build
 echo "OrderService"
-cd ../OrderService
+cd ./OrderService
 ./gradlew build
 echo "ProductService"
 cd ../ProductService
