5b036e614b41dc8cc8646b81ac2352c4b561f601
poringbom <poringbom@PoringbomZA>
2017-03-05 15:45:57 +0700

    update

--- OrderService/src/main/java/com/sprint3r/deans4j/CustomerRespoitory.java
+++ OrderService/src/main/java/com/sprint3r/deans4j/CustomerRespoitory.java
@@ -3,7 +3,7 @@ package com.sprint3r.deans4j;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
 public interface CustomerRespoitory extends  CrudRepository<Customer,Long>{
 
 }
--- OrderService/src/main/java/com/sprint3r/deans4j/OrderRepository.java
+++ OrderService/src/main/java/com/sprint3r/deans4j/OrderRepository.java
@@ -3,7 +3,7 @@ package com.sprint3r.deans4j;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "order", path = "order")
 public interface  OrderRepository extends  CrudRepository<Order,Long>{
 
 }
