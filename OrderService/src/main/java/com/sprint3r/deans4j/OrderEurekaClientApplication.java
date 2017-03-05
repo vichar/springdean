2c52cfcf5fe7e59e93ecf63dd155d9c6a424f72e
poringbom <poringbom@PoringbomZA>
2017-03-05 15:54:10 +0700

    update

--- OrderService/src/main/java/com/sprint3r/deans4j/OrderEurekaClientApplication.java
+++ OrderService/src/main/java/com/sprint3r/deans4j/OrderEurekaClientApplication.java
@@ -2,10 +2,14 @@ package com.sprint3r.deans4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
 public class OrderEurekaClientApplication {
 
 	public static void main(String[] args) {
