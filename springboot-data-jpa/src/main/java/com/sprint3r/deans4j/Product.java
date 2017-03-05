dc631b86fddcd13bd2e47089005c864cd0d10c7e
Akadet Mathurapoj <akadet85@gmail.com>
2017-03-05 14:33:13 +0700

    [Top&Pun] product add image url

--- springboot-data-jpa/src/main/java/com/sprint3r/deans4j/Product.java
+++ springboot-data-jpa/src/main/java/com/sprint3r/deans4j/Product.java
@@ -13,19 +13,21 @@ public class Product {
     private Long id;
     private String name;
     private String type;
+    private String imageUrl;
     private String description;
     private double price;
 
     protected Product() {
     }
 
-    public Product(String name, String type, String description, double price) {
-        this(null, name, type, description, price);
+    public Product(String name, String type, String imageUrl, String description, double price) {
+        this(null, name, imageUrl, type, description, price);
     }
 
-    Product(Long id, String name, String type, String description, double price) {
+    Product(Long id, String name, String imageUrl, String type, String description, double price) {
         this.id = id;
         this.name = name;
+        this.imageUrl = imageUrl;
         this.type = type;
         this.description = description;
         this.price = price;
--- springboot-data-jpa/src/main/java/com/sprint3r/deans4j/SpringbootDataJpaApplication.java
+++ springboot-data-jpa/src/main/java/com/sprint3r/deans4j/SpringbootDataJpaApplication.java
@@ -8,17 +8,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringbootDataJpaApplication {
 
-	@Bean
-	public InitializingBean seedDatabase(ProductRepository repository) {
-		return () -> {
-			repository.save(new Product("Intel i7", "CPU", "i7 7700k", 7000));
-			repository.save(new Product("AMD Raizen 1700X", "CPU", "AMD 1700X",3500));
-			repository.save(new Product("Galax HOF GTX1080", "GPU", "GTX 1080",2350));
-			repository.save(new Product("Raiden Plasma","RAM", "DDR4 32GB",8872));
-		};
-	}
+    @Bean
+    public InitializingBean seedDatabase(ProductRepository repository) {
+        return () -> {
+            repository.save(new Product("Intel i7", "CPU", "https://i.ytimg.com/vi/mmHY8Kj4vCk/maxresdefault.jpg", "i7 7700k", 7000));
+            repository.save(new Product("AMD Raizen 1700X", "CPU", "http://cdn.wccftech.com/wp-content/uploads/2017/02/AMD-Ryzen-CPU.png", "AMD 1700X", 3500));
+            repository.save(new Product("Galax HOF GTX1080", "GPU", "https://www.custompcreview.com/wp-content/uploads/2016/05/29140448720l.jpg", "GTX 1080", 2350));
+            repository.save(new Product("Raiden Plasma", "RAM", "https://i.ytimg.com/vi/SApyTXm_jQE/maxresdefault.jpg", "DDR4 32GB", 8872));
+        };
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDataJpaApplication.class, args);
+    }
 }
