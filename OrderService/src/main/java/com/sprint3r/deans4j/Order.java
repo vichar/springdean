0c8917021b4719f34375ecf371a6ce00d86ce817
poringbom <poringbom@PoringbomZA>
2017-03-05 14:35:37 +0700

    update

--- OrderService/src/main/java/com/sprint3r/deans4j/Order.java
+++ OrderService/src/main/java/com/sprint3r/deans4j/Order.java
@@ -12,4 +12,42 @@ public class Order {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	private String name;
+	
+	private Float price;
+	
+	private String desc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
 }
